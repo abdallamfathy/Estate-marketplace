a08769b2a61ead08dbe8f133f5b89484f325c2b0
abdallamfathy <abdallamfathy@gmail.com>
2022-10-06 16:39:34 +0200

    Add sign in form

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,43 +1,73 @@
-import { useNavigate , useLocation } from 'react-router-dom'
-import { ReactComponent as OfferIcon } from '../assets/svg/localOfferIcon.svg'
-import { ReactComponent as ExploreIcon } from '../assets/svg/exploreIcon.svg'
-import { ReactComponent as PersonOutlineIcon } from '../assets/svg/personOutlineIcon.svg'
+import { useNavigate, useLocation } from "react-router-dom";
+import { ReactComponent as OfferIcon } from "../assets/svg/localOfferIcon.svg";
+import { ReactComponent as ExploreIcon } from "../assets/svg/exploreIcon.svg";
+import { ReactComponent as PersonOutlineIcon } from "../assets/svg/personOutlineIcon.svg";
 
 const Navbar = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
 
-    const navigate = useNavigate()
-    const location = useLocation()
-
-    const pathMatch = (route) => {
-        if (location.pathname === route) {
-            return true;
-        }
+  const pathMatch = (route) => {
+    if (location.pathname === route) {
+      return true;
     }
+  };
 
   return (
-    <footer className='navbar'>
-        <nav className="navbarNav">
-            <ul className="navbarListItems">
-                <li onClick={() => navigate("/")}
-                className="navbarListItem">
-                    <ExploreIcon fill={pathMatch("/") ? "#2c2c2c" : "#8f8f8f"} width="36" height="36px"
-                     />
-                    <p className={pathMatch("/") ? "navbarListItemNameActive" : "navbarListItemName"}>Explore</p>
-                </li>
-                <li onClick={() => navigate("/offers")}className="navbarListItem">
-                    <OfferIcon width="36" height="36px"
-                     fill={pathMatch("/offers") ? "#2c2c2c" : "#8f8f8f"}
-                     />
-                    <p className={pathMatch("/offers") ? "navbarListItemNameActive" : "navbarListItemName"}>Offer</p>
-                </li>
-                <li onClick={() => navigate("/profile")}className="navbarListItem">
-                    <PersonOutlineIcon fill={pathMatch("/profile") ? "#2c2c2c" : "#8f8f8f"} width="36" height="36px"
-                     />
-                    <p className={pathMatch("/profile") ? "navbarListItemNameActive" : "navbarListItemName"}>Profile</p>
-                </li>
-            </ul>
-        </nav>
+    <footer className="navbar">
+      <nav className="navbarNav">
+        <ul className="navbarListItems">
+          <li onClick={() => navigate("/")} className="navbarListItem">
+            <ExploreIcon
+              fill={pathMatch("/") ? "#2c2c2c" : "#8f8f8f"}
+              width="36"
+              height="36px"
+            />
+            <p
+              className={
+                pathMatch("/")
+                  ? "navbarListItemNameActive"
+                  : "navbarListItemName"
+              }
+            >
+              Explore
+            </p>
+          </li>
+          <li onClick={() => navigate("/offers")} className="navbarListItem">
+            <OfferIcon
+              width="36"
+              height="36px"
+              fill={pathMatch("/offers") ? "#2c2c2c" : "#8f8f8f"}
+            />
+            <p
+              className={
+                pathMatch("/offers")
+                  ? "navbarListItemNameActive"
+                  : "navbarListItemName"
+              }
+            >
+              Offer
+            </p>
+          </li>
+          <li onClick={() => navigate("/profile")} className="navbarListItem">
+            <PersonOutlineIcon
+              fill={pathMatch("/profile") ? "#2c2c2c" : "#8f8f8f"}
+              width="36"
+              height="36px"
+            />
+            <p
+              className={
+                pathMatch("/profile")
+                  ? "navbarListItemNameActive"
+                  : "navbarListItemName"
+              }
+            >
+              Profile
+            </p>
+          </li>
+        </ul>
+      </nav>
     </footer>
-  )
-}
-export default Navbar
+  );
+};
+export default Navbar;
--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,8 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg';
+import { visibilityIcon } from '../assets/svg/visibilityIcon.svg';
+import { Link , useNavigate } from 'react-router-dom'
+
 
 const SignIn = () => {
+  const [showPassword , setShowPassword] = useState(false)
+
+  const [formData,  setformData] = useState({
+    email: "",
+    password: "",
+  })
+  const {email , password} = formData
+  
+  const onChange = () => {
+    return true
+  }
+
+  const navigate = useNavigate();
+
   return (
-    <div>SignIn</div>
+    <>
+    <div className="pageContainer">
+      <header>
+        <p className="pageHeader">
+          Welcome Back!
+        </p>
+      </header>
+      <main>
+        <form>
+          <input type="email" className="emailInput" placeholder='Email' id="email" value={email} onChange={onChange} />
+          <div className="passwordInputDiv">
+            <input
+            className='passwordInput' type={showPassword ? "text" : "password"}
+            id="password"
+            placeholder='password'
+            value={password}
+            onChange={onChange} />
+          </div>
+
+          <img src={visibilityIcon} alt="showpassowrd" onClick={setShowPassword((prevState) => !prevState ) } />
+        </form>
+      </main>
+    </div>
+    </>
   )
 }
 
