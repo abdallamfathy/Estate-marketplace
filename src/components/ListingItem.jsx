18d5945eb144762b5afa8ea01634c81878a4b5c6
abdallamfathy <abdallamfathy@gmail.com>
2022-10-14 14:15:39 +0200

    Add edit icon ui

--- src/components/ListingItem.jsx
+++ src/components/ListingItem.jsx
@@ -1,10 +1,11 @@
 import { ReactComponent as DeleteIcon } from "../assets/svg/deleteIcon.svg"
+import { ReactComponent as EditIcon } from "../assets/svg/editIcon.svg"
 import bedIcon from "../assets/svg/bedIcon.svg"
 import bathtubIcon from "../assets/svg/bathtubIcon.svg"
 import { Link } from "react-router-dom"
 
 
-const ListingItem = ({listing , id , onDelete}) => {
+const ListingItem = ({listing , id , onDelete , onEdit}) => {
   return (
     <li className="categoryListing">
         <Link to={`/category/${listing.type}/${id}`} className="categoryListingLink">
@@ -34,6 +35,10 @@ const ListingItem = ({listing , id , onDelete}) => {
         </Link>
         {onDelete && (
             <DeleteIcon className="removeIcon" fill="rgb(231,76,60)" onClick={()=> onDelete(listing.id, listing.name)}/>
+        )}
+
+{onEdit&& (
+            <EditIcon className="editIcon" fill="rgb(231,76,60)" onClick={()=> onEdit(id)}/>
         )}
     </li>
   )
